3d0cd7202506e7ef07ff2e6ffafaee305c44e1be
Dora2021Chen <Dora2021.Chen@outlook.com>
2021-08-22 19:14:35 -0600

    DB connection pool configuration

--- src/main/java/com/workerMonitor/api/repository/SystemLogRepository.java
+++ src/main/java/com/workerMonitor/api/repository/SystemLogRepository.java
@@ -4,6 +4,7 @@ import com.workerMonitor.api.model.SystemLogModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
